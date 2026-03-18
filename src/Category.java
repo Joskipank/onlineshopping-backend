2ce3169a5d858c6bc078d5ba6c2f13504bdef61a
Joskipank <magaiminov8@gmail.com>
2026-03-18 18:03:54 +0300

    init commit

---
+++ src/Category.java
@@ -0,0 +1,4 @@
+public abstract class Category extends Product {
+    private category
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Product product1 = new Product("xxx", 322.9, );
+        System.out.println(product1.getPrice() + " " + product1.getId() + " " + product1.getTitle());
+
+    }
+}
